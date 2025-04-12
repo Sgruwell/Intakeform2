1b011351deb8f50786e9548770cf8afd1563a89c
Sgruwell <sean@datascore.ai>
2025-04-12 07:45:25 -0400

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import IntakeForm from './components/IntakeForm'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <IntakeForm />
+  </React.StrictMode>,
+)
